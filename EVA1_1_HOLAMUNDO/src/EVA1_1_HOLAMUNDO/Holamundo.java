579777e18d12727265c2e330bbbf76fa1bb865d2
IramMcFly <124701863+IramMcFly@users.noreply.github.com>
2023-02-07 09:45:34 -0600

    Add files via upload

---
+++ EVA1_1_HOLAMUNDO/src/EVA1_1_HOLAMUNDO/Holamundo.java
@@ -0,0 +1,14 @@
+package EVA1_1_HOLAMUNDO;
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo!!");
+		
+	}
+
+}
+
+
+//propiedad de Iram Ballesteros M.
