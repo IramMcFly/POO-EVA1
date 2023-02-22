bedad985836dbbc2d1524573040e321e2a129b4b
IramMcFly <124701863+IramMcFly@users.noreply.github.com>
2023-02-22 09:37:17 -0600

    Add files via upload

---
+++ EVA1_11_RETURN/src/RETURNS/EVA1_11_RETURN.java
@@ -0,0 +1,15 @@
+package RETURNS;
+
+public class EVA1_11_RETURN {
+
+	public static void main(String[] args) {
+		
+		int res = sqrt(10);
+		System.out.print(res);
+	}
+	
+	public static int sqrt(int num) {
+		return num * num;
+	}
+
+}
