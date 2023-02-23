04adb986730b621e73e60930d71e4a4ce38fbd8c
IramMcFly <124701863+IramMcFly@users.noreply.github.com>
2023-02-23 09:49:22 -0600

    Add files via upload

---
+++ EVA1_15_SobreCarga/src/Sobrecarga/EVA1_15_SobreCarga.java
@@ -0,0 +1,25 @@
+package Sobrecarga;
+
+public class EVA1_15_SobreCarga {
+
+	public static void main(String[] args) {
+		
+		/*
+		 *Diferentes println
+		 *son 10 diferentes
+		 *para imprimir cada uno de los tipos de datos
+		 *  
+		 *Para crear sobrecarga de metodos
+		 *la firma del metodo debe ser unica
+		 *
+		 */
+		
+		System.out.println("Hola mundo");
+		System.out.println(1000);
+		System.out.println(true);
+		System.out.println(args);
+		System.out.println();
+
+	}
+
+}
